47731933c05abd302afea8e0df22501fbf967bb5
mahimahamsa <pji.mahima.hamsa@kalvium.community>
2024-06-24 18:34:38 +0530

    new

--- src/app.js
+++ src/app.js
@@ -35,9 +35,9 @@ function renderAll() {
   renderPrice();
 }
 
+// Rendering functions for each ingredient
 function renderPatty() {
   let patty = document.querySelector("#patty");
-  //you can also use getElementById
   if (state.Patty) {
     patty.style.display = "inherit";
   } else {
@@ -46,44 +46,110 @@ function renderPatty() {
 }
 
 function renderCheese() {
-  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese");
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
-  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomatoes = document.querySelector("#tomato");
+  if (state.Tomatoes) {
+    tomatoes.style.display = "inherit";
+  } else {
+    tomatoes.style.display = "none";
+  }
 }
 
 function renderOnions() {
-  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onions = document.querySelector("#onion");
+  if (state.Onions) {
+    onions.style.display = "inherit";
+  } else {
+    onions.style.display = "none";
+  }
 }
 
 function renderLettuce() {
-  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce");
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+  }
 }
 
+// This function renders the entire screen everytime the state changes accordingly
+function renderAll() {
+  renderPatty();
+  renderCheese();
+  renderTomatoes();
+  renderOnions();
+  renderLettuce();
+  renderButtons();
+  renderIngredientsBoard();
+  renderPrice();
+}
 document.querySelector(".btn-patty").onclick = function () {
   state.Patty = !state.Patty;
   renderAll();
 };
 
-// Trial 2 - Setup event listener for the cheese button
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
+
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
+
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
+
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 
-// Trial 2 - Setup event listener for the tomatoes button
+
+function renderButtons() {
+  document.querySelector(".btn-patty").classList.toggle("active", state.Patty);
+  document.querySelector(".btn-cheese").classList.toggle("active", state.Cheese);
+  document.querySelector(".btn-tomatoes").classList.toggle("active", state.Tomatoes);
+  document.querySelector(".btn-onions").classList.toggle("active", state.Onions);
+  document.querySelector(".btn-lettuce").classList.toggle("active", state.Lettuce);
+}
+
+function renderIngredientsBoard() {
+  let ingredientsBoard = document.querySelector(".menu-container");
+  let selectedIngredients = "";
+  
+  for (let ingredient in state) {
+    if (state[ingredient]) {
+      selectedIngredients += `<p class="items">${ingredient}</p>`;
+    }
+  }
+  ingredientsBoard.innerHTML = `<h3>Ingredients</h3>${selectedIngredients}`;
+}
 
 
-// Trial 2 - Setup event listener for the onion button
+function renderPrice() {
+  let totalPrice = wholeWheatBun;
+  for (let ingredient in state) {
+    if (state[ingredient]) {
+      totalPrice += ingredients[ingredient];
+    }
+  }
+  document.querySelector(".price-details").textContent = `INR ${totalPrice}`;
+}
 
+// Initial render
+renderAll();
 
-// Trial 2 - Setup event listener for the lettuce button
-
-
-//Challenge 1 - Add/Remove the class active to the buttons based on state
-
-
-//Challenge 2 - Render only the items selected in the ingredients board based on the state
-
-
-//Judgement 1
-//In the p element having price-details as the class, display the calculated
-//price based on ingredients
